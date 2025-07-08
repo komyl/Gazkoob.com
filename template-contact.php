e086d3e4daf8580f6187d2843402242a3f4f8f06
Komeyl Kalhorinia <komylfa@gmail.com>
2025-07-08 12:25:09 +0330

    fix Maps

--- template-contact.php
+++ template-contact.php
@@ -45,13 +45,13 @@ get_header();
             <div class="map-container">
                 <h4>موقعیت ما در گوگل مپ</h4>
                 <div class="map-iframe-wrapper">
-                    <iframe class="lazy-map" title="نقشه گوگل موقعیت مجموعه گازکوب" data-src="https://storage.googleapis.com/maps-solutions-(Your_APi_Key)/locator-plus/eb7g/locator-plus.html" width="100%" height="100%" style="border:0;" loading="lazy"></iframe>
+                    <iframe class="lazy-map" title="نقشه گوگل موقعیت مجموعه گازکوب" data-src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3212.519856358085!2d59.51986227500854!3d36.372407372372095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f6c8c542f0fac19%3A0x5b9430192d9417b5!2sDariush%20Car%20Wash!5e0!3m2!1sen!2sde!4v1751964685770!5m2!1sen!2sde" width="100%" height="100%" style="border:0;" loading="lazy"></iframe>
                 </div>
             </div>
             <div class="map-container">
                 <h4>موقعیت ما در نقشه نشان</h4>
                 <div class="map-iframe-wrapper">
-                    <iframe class="lazy-map" title="نقشه نشان موقعیت مجموعه گازکوب" data-src="https://nshn.ir/_(Your_APi_Key)?iframe=true" width="100%" height="100%" style="border:0;" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
+                    <iframe class="lazy-map" title="نقشه نشان موقعیت مجموعه گازکوب" data-src="https://neshan.org/maps/iframe/places/07c38e89c379137a305a82b83518d958#c36.372-59.525-16z-0p/36.3722511/59.52163990000001" width="100%" height="100%" style="border:0;" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
                 </div>
             </div>
         </div>
